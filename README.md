9abb67e975fe20baac93b8997893786a6156afdb
jgoldford <goldford.joshua@gmail.com>
2021-02-02 10:42:34 -0500

    added readme/setup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ASR
 python package for ancestral sequence reconstruction pipeline
+
+
+PIPELINE For the construction for ancestral sequences based on the tutorial provided by Rubin Studer:
+https://evosite3d.blogspot.com/2014/09/tutorial-on-ancestral-sequence.html
+
+Dependence all on path and aliased
+mafft - multiple sequence alignment
+phyml - large tree building using maximum likelihood
+codeml - (i.e. PAML) ancestral state reconstruction
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+	name = "ASR",
+	version = '0.0',
+	description = 'Python package containing pipeline for ancestral sequence reconstruction',
+	url = 'https://github.com/jgoldford/ASR/',
+	author = 'Joshua E. Goldford',
+	author_email = 'goldford.joshua@gmail.com',
+	packages = find_packages(),
+	install_requires = [
+		'scipy',
+		'numpy',
+		'pandas'
+		'biopython'],
+	include_package_data = True,
+)
